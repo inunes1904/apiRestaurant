202d6ea3a06a97ad736c1b72de2a3306094ddffa
Ivo Nunes <inunes1904@gmail.com>
2023-05-18 21:43:07 +0100

    first commit

---
+++ src/main/java/ivonunes/com/apiRestaurant/ApiRestaurantApplication.java
@@ -0,0 +1,15 @@
+package ivonunes.com.apiRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ApiRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ivonunes/com/apiRestaurant/ApiRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package ivonunes.com.apiRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
